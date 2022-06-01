2e6f4290894536de3cd3f739f5407d45bb764a29
Erik <Mr.erik.egliens@gmail.com>
2022-06-01 23:27:42 +0300

    added blobs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import FirstPage from "./components/FirstPage/FirstPage";
+import Blob from "./components/Blob/Blob";
 
 function App() {
   return (
     <>
+      <Blob color={"yellow"}/>
       <FirstPage />
+      <Blob color={"blue"}/>
     </>
   );
 }
