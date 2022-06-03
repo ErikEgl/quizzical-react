9d34b58313640a43c02d33b15ba59718c01dcbe6
Erik <Mr.erik.egliens@gmail.com>
2022-06-03 20:10:49 +0300

    reorganized buttons rendering place

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Button from "../Button/Button";
 
-function Question({question, correct_answer, incorrect_answers}) {
+function Question({question}) {
   return (
-    <>
-      <div className="question">
+    <>      
         <h2>{question}</h2>
-        <Button className='button-secondary' content={correct_answer} />
-      </div>
     </>
   );
 }
