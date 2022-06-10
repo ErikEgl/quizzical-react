b4566e72f438be0fdcd29999459c6bccd65e24a1
Erik <Mr.erik.egliens@gmail.com>
2022-06-10 17:51:28 +0300

    added id

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   return (
     <>
-      <button onClick={props.holdAnswer ? props.holdAnswer :  props.onClick}  className={props.className}>{props.content} {props.answer}</button>
+      <button id={props.id} onClick={props.holdAnswer ? props.holdAnswer :  props.onClick}  className={props.className}>{props.content} {props.answer}</button>
     </>
   );
 }
