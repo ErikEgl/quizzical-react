d5625ce4778f867fb05e9d7911ae547922f109e6
Erik <Mr.erik.egliens@gmail.com>
2022-06-10 18:23:47 +0300

    disabled attr after game is over

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 function Button(props) {
   return (
     <>
-      <button id={props.id} onClick={props.holdAnswer ? props.holdAnswer :  props.onClick}  className={props.className}>{props.content} {props.answer}</button>
+      <button disabled={props.disabled} id={props.id} onClick={props.holdAnswer ? props.holdAnswer :  props.onClick}  className={props.className}>{props.content} {props.answer}</button>
     </>
   );
 }
