33bcba6968ee4ff860b0d5cd31b669cf84a1f714
Erik <Mr.erik.egliens@gmail.com>
2022-10-01 18:21:12 +0300

    quiz options data

---
+++ src/components/QuizOptionsSelect/quizOptionsData.js
@@ -0,0 +1,104 @@
+export default {
+  options: [
+    {
+      value: "any",
+      title: "Any Category",
+    },
+    {
+      value: 9,
+      title: "General Knowledge",
+    },
+    {
+      value: 10,
+      title: "Entertainment: Books",
+    },
+    {
+      value: 11,
+      title: "Entertainment: Film",
+    },
+    {
+      value: 12,
+      title: "Entertainment: Music",
+    },
+    {
+      value: 13,
+      title: "Entertainment: Musicals Theatres",
+    },
+    {
+      value: 14,
+      title: "Entertainment: Television",
+    },
+    {
+      value: 15,
+      title: "Entertainment: Video Games",
+    },
+    {
+      value: 16,
+      title: "Entertainment: Board Games",
+    },
+    {
+      value: 17,
+      title: "Science Nature",
+    },
+    {
+      value: 18,
+      title: "Science: Computers",
+    },
+    {
+      value: 19,
+      title: "Science: Mathematics",
+    },
+    {
+      value: 20,
+      title: "Mythology",
+    },
+    {
+      value: 21,
+      title: "Sports",
+    },
+    {
+      value: 22,
+      title: "Geography",
+    },
+    {
+      value: 23,
+      title: "History",
+    },
+    {
+      value: 24,
+      title: "Politics",
+    },
+    {
+      value: 25,
+      title: "Art",
+    },
+    {
+      value: 26,
+      title: "Celebrities",
+    },
+    {
+      value: 27,
+      title: "Animals",
+    },
+    {
+      value: 28,
+      title: "Vehicles",
+    },
+    {
+      value: 29,
+      title: "Entertainment: Comics",
+    },
+    {
+      value: 30,
+      title: "Science: Gadgets",
+    },
+    {
+      value: 31,
+      title: "Entertainment: Japanese Anime Manga",
+    },
+    {
+      value: 32,
+      title: "Entertainment: Cartoon Animations",
+    },
+  ],
+};
