cafcf6da232ce81cf0921780a1b5992d19caaffd
Erik <Mr.erik.egliens@gmail.com>
2022-10-01 18:22:11 +0300

    AppContextProvider added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { AppContextProvider } from "./utils/useContext";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+<AppContextProvider>
+    <App />
+</AppContextProvider>
+);
 
 
